729db1ec2d37d27799bad5674b5435f30d498a86
Mohamed Abdallah <110228201+MAzewail@users.noreply.github.com>
2023-08-10 11:03:46 +0300

    Update millimeter_scope_device.ino

--- millimeter_scope_device.ino
+++ millimeter_scope_device.ino
@@ -1,10 +1,7 @@
-// C++ code
-//
 #define trig 9
 #define echo 12
-long d=0,t=0;
 byte pins[8]={8,7,6,5,4,3,2,1};
-byte disp[10][8] = {
+byte disp[11][8] = {
   {1, 1, 1, 1, 1, 1, 0, 0}, // 0
   {0, 1, 1, 0, 0, 0, 0, 0}, // 1
   {1, 1, 0, 1, 1, 0, 1, 0}, // 2
@@ -14,27 +11,29 @@ byte disp[10][8] = {
   {1, 0, 1, 1, 1, 1, 1, 0}, // 6
   {1, 1, 1, 0, 0, 0, 0, 0}, // 7
   {1, 1, 1, 1, 1, 1, 1, 0}, // 8
-  {1, 1, 1, 1, 0, 1, 1, 0} // 9
+  {1, 1, 1, 1, 0, 1, 1, 0}, // 9
+  {0, 0, 0, 0, 0, 0, 0, 0}  // Null
 };
 
 void presNum(byte num){
   for(int i=0;i<sizeof(pins);i++)
     digitalWrite(*(pins+i),*(*(disp+num)+i)); 
 }
-void ultrasonic(){
+long ultrasonic(){
+  long t=0;
   digitalWrite(trig,0);
   delayMicroseconds(2);
   digitalWrite(trig,1);
   delayMicroseconds(10);
   digitalWrite(trig,0);
   t=pulseIn(echo,1);
-  d=t*0.0343/2;
+  return t*0.0343/2;
 }
 
 void setup()
 {
   for(int i=0;i<9;i++)
-    pinMode(pins[i],OUTPUT);
+  pinMode(pins[i],OUTPUT);
   pinMode(trig,OUTPUT);
   pinMode(echo,INPUT);
   Serial.begin(9600);
@@ -42,14 +41,5 @@ void setup()
 
 void loop()
 {
-	ultrasonic();
-  Serial.println(d);
-  presNum((d<100)?(int(float(d)/10.0)):0);
-  //if(d<100)presNum(int(float(d)/10.0));
-  
+  presNum((ultrasonic()<100)?(ultrasonic()/10):10);
 }
-
-
-
-
-
